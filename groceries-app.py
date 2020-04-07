a0acc9bb9afeede278dd289aa800f3845c1130a8
Jeff Cook <jecook2692@gmail.com>
2020-04-06 22:37:59 -0400

    adds user prompt

--- groceries-app.py
+++ groceries-app.py
@@ -1,3 +1,10 @@
 #This program will create a groceries app that will allow the user to add, update,and remove items.
 
 groceries = []
+
+# Create a while loop that will prompt the user until they hit Enter.
+while True:
+    item = input("What do you need from the store? ")
+    if item == '':
+        break
+    groceries.append(item)
